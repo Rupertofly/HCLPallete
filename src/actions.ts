c1181593457ce5b93473529f76444e77f330e71f
Ruby Quail <ruby@rubyquail.design>
2020-06-02 20:48:24 +1000

    store and state
    Signed-off-by: Ruby Quail <ruby@rubyquail.design>

---
+++ src/actions.ts
@@ -0,0 +1,191 @@
+import { PalleteImport } from './state';
+
+//#region Colour Actions
+export enum COLOUR_ACTION_TYPES {
+  SET_VAL = 'setValue',
+  SET_COLOR = 'setColor',
+}
+
+// Set Colour
+type setColourInput = string | { h: number; c: number; l: number };
+export function setColour(color: setColourInput, shade: number, hue: number) {
+  return {
+    actionType: 'COLOUR',
+    action: {
+      type: COLOUR_ACTION_TYPES.SET_COLOR,
+      options: {
+        shade,
+        hue,
+        color,
+      },
+    },
+  } as const;
+}
+export type ActionSetColour = ReturnType<typeof setColour>;
+
+// Set Value
+type valueType = 'h' | 'c' | 'l';
+export function setValue(opts: { hue: number; shade: number; property: valueType; value: number }) {
+  return {
+    actionType: 'COLOUR',
+    action: {
+      type: COLOUR_ACTION_TYPES.SET_VAL,
+      options: {
+        ...opts,
+      },
+    },
+  } as const;
+}
+export type ActionSetValue = ReturnType<typeof setValue>;
+
+export type ColourActions = ActionSetColour | ActionSetValue;
+//#endregion
+//#region Pallete Actions
+
+export enum PALLETE_ACTION_TYPES {
+  ADD_LAYER = 'addLayer',
+  REMOVE_LAYER = 'removeLayer',
+  REARRANGE_LAYER = 'rearrangeLayer',
+  RENAME_LAYER = 'renameLayer',
+  CALCULATE_LAYER = 'calculateLayer',
+}
+type layers = 'hue' | 'shade';
+// Add Layer
+export function addLayer(layerType: layers) {
+  return {
+    actionType: 'PALLETE',
+    action: {
+      type: PALLETE_ACTION_TYPES.ADD_LAYER,
+      options: {
+        type: layerType,
+      },
+    },
+  } as const;
+}
+export type ActionAddLayer = ReturnType<typeof addLayer>;
+// Remove Layer
+export function removeLayer(layerType: layers, index: number) {
+  return {
+    actionType: 'PALLETE',
+    action: {
+      type: PALLETE_ACTION_TYPES.REMOVE_LAYER,
+      options: {
+        type: layerType,
+        index,
+      },
+    },
+  } as const;
+}
+export type ActionRemoveLayer = ReturnType<typeof removeLayer>;
+// Rearrange Layer
+export function rearrangeLayer(layerType: layers, from: number, to: number) {
+  return {
+    actionType: 'PALLETE',
+    action: {
+      type: PALLETE_ACTION_TYPES.REARRANGE_LAYER,
+      options: {
+        type: layerType,
+        from,
+        to,
+      },
+    },
+  } as const;
+}
+export type ActionRearrangeLayer = ReturnType<typeof rearrangeLayer>;
+// Rename Layer
+export function renameLayer(layerType: layers, index: number, newName: string) {
+  return {
+    actionType: 'PALLETE',
+    action: {
+      type: PALLETE_ACTION_TYPES.RENAME_LAYER,
+      options: {
+        type: layerType,
+        index,
+        newName,
+      },
+    },
+  } as const;
+}
+export type ActionRenameLayer = ReturnType<typeof renameLayer>;
+
+export function calculateLayer(layerType: layers, index: number) {
+  return {
+    actionType: 'PALLETE',
+    action: {
+      type: PALLETE_ACTION_TYPES.CALCULATE_LAYER,
+      options: {
+        type: layerType,
+        index,
+      },
+    },
+  } as const;
+}
+export type ActionCalculateLayer = ReturnType<typeof calculateLayer>;
+
+export type PalleteActions =
+  | ActionAddLayer
+  | ActionCalculateLayer
+  | ActionRearrangeLayer
+  | ActionRemoveLayer
+  | ActionRenameLayer;
+//#endregion
+//#region Global Actions
+export enum GLOBAL_ACTION_TYPES {
+  LOAD_PALLETE = 'loadPallete',
+  SAVE_PALLETE = 'savePallete',
+  RENAME_PALLETE = 'renamePallete',
+  IMPORT_PALLETE = 'importPallete',
+  REBUILD = 'rebuild',
+}
+// Load Pallete
+export function loadPallete(name: string) {
+  return {
+    actionType: 'GLOBAL',
+    action: {
+      type: GLOBAL_ACTION_TYPES.LOAD_PALLETE,
+      options: {
+        name,
+      },
+    },
+  } as const;
+}
+export type ActionLoadPallete = ReturnType<typeof loadPallete>;
+
+// Save Pallete
+export function savePallete() {
+  return {
+    actionType: 'GLOBAL',
+    action: {
+      type: GLOBAL_ACTION_TYPES.SAVE_PALLETE,
+      options: {},
+    },
+  } as const;
+}
+export type ActionSavePallete = ReturnType<typeof savePallete>;
+
+// Rename Pallete
+export function renamePallete(newName: string) {
+  return {
+    actionType: 'GLOBAL',
+    action: {
+      type: GLOBAL_ACTION_TYPES.RENAME_PALLETE,
+      options: {
+        newName,
+      },
+    },
+  } as const;
+}
+export type ActionRenamePallete = ReturnType<typeof renamePallete>;
+// Import Pallete
+export function importPallete(toImport: PalleteImport) {
+  return {
+    actionType: 'GLOBAL',
+    action: {
+      type: GLOBAL_ACTION_TYPES.IMPORT_PALLETE,
+      options: {
+        toImport,
+      },
+    },
+  } as const;
+}
+export type ActionImportPallete = ReturnType<typeof importPallete>;
--- src/types.ts
+++ src/types.ts
@@ -1,178 +1,178 @@
 export type HCLProp = 'h' | 'c' | 'l';
 export interface ColorLocator {
-    hue: number;
-    shade: number;
+  hue: number;
+  shade: number;
 }
 interface UIColours extends Readonly<any> {
-    readonly DARK_COL: string;
-    readonly LIGHT_COL: string;
+  readonly DARK_COL: string;
+  readonly LIGHT_COL: string;
 }
 export enum UICOLOURS {
-    DARK_COL = '#373737',
-    LIGHT_COL = '#f0f0f0',
+  DARK_COL = '#373737',
+  LIGHT_COL = '#f0f0f0',
 }
 
 export interface ValueLocatior extends ColorLocator {
-    p: HCLProp;
+  p: HCLProp;
 }
-export interface Col {
+export interface Colour {
+  h: number;
+  c: number;
+  l: number;
+  hex: string;
+  light: boolean;
+  r: {
     h: number;
     c: number;
     l: number;
-    hex: string;
-    light: boolean;
-    r: {
-        h: number;
-        c: number;
-        l: number;
-    };
+  };
 }
 export interface HueInfo {
-    name: string;
-    avgHue: number;
+  name: string;
+  avgHue: number;
 }
 export interface ShadeInfo {
-    name: string;
-    avgValue: number;
+  name: string;
+  avgValue: number;
 }
 export interface State {
-    shades: ShadeInfo[];
-    hues: HueInfo[];
-    colours: Col[][];
+  shades: ShadeInfo[];
+  hues: HueInfo[];
+  colours: Colour[][];
 }
 export interface JSONPallete {
-    shades: string[];
-    hues: string[];
-    colours: JSONCol[][];
+  shades: string[];
+  hues: string[];
+  colours: JSONCol[][];
 }
 interface JSONCol {
-    h: number;
-    c: number;
-    l: number;
+  h: number;
+  c: number;
+  l: number;
 }
 // module 'ntcjs' {
 //     const content: any;
 //     export default content;
 // }
 export enum COL_PROPS {
-    h = 'h',
-    c = 'c',
-    l = 'l',
+  h = 'h',
+  c = 'c',
+  l = 'l',
 }
 export enum ACTION_TYPES {
-    SET_VAL = 'setValue',
-    SET_COLOR = 'setColor',
-    ADD_LAYER = 'addLayer',
-    REMOVE_LAYER = 'removeLayer',
-    RENAME = 'rename',
-    LOAD_PALLETE = 'loadPallete',
-    SAVE_PALLETE = 'savePallete',
-    IMPORT_PALLETE = 'importPallete',
-    REBUILD = 'rebuild',
-    CALCULATE_HUE = 'calculateHue',
-    CALCULATE_SHADE = 'calculateShade',
+  SET_VAL = 'setValue',
+  SET_COLOR = 'setColor',
+  ADD_LAYER = 'addLayer',
+  REMOVE_LAYER = 'removeLayer',
+  RENAME = 'rename',
+  LOAD_PALLETE = 'loadPallete',
+  SAVE_PALLETE = 'savePallete',
+  IMPORT_PALLETE = 'importPallete',
+  REBUILD = 'rebuild',
+  CALCULATE_HUE = 'calculateHue',
+  CALCULATE_SHADE = 'calculateShade',
 }
 export interface ActionBaseRequest {
-    type: ACTION_TYPES;
-    data: Record<string, any>;
+  type: ACTION_TYPES;
+  data: Record<string, any>;
 }
 export interface SetValActionRequest {
-    type: ACTION_TYPES.SET_VAL;
-    data: {
-        shade: number;
-        hue: number;
-        prop: COL_PROPS;
-        value: number;
-    };
+  type: ACTION_TYPES.SET_VAL;
+  data: {
+    shade: number;
+    hue: number;
+    prop: COL_PROPS;
+    value: number;
+  };
 }
 export interface SetColorActionRequest {
-    type: ACTION_TYPES.SET_COLOR;
-    data: {
-        shade: number;
-        hue: number;
-        color: string;
-    };
+  type: ACTION_TYPES.SET_COLOR;
+  data: {
+    shade: number;
+    hue: number;
+    color: string;
+  };
 }
 export interface AddLayerActionRequest {
-    type: ACTION_TYPES.ADD_LAYER;
-    data: {
-        layerType: 'hue' | 'shade';
-        name: string;
-    };
+  type: ACTION_TYPES.ADD_LAYER;
+  data: {
+    layerType: 'hue' | 'shade';
+    name: string;
+  };
 }
 export interface RemoveLayerActionRequest {
-    type: ACTION_TYPES.REMOVE_LAYER;
-    data: {
-        layerType: 'hue' | 'shade';
-        index: number;
-    };
+  type: ACTION_TYPES.REMOVE_LAYER;
+  data: {
+    layerType: 'hue' | 'shade';
+    index: number;
+  };
 }
 
 export interface RenameActionRequest {
-    type: ACTION_TYPES.RENAME;
-    data: {
-        layerType: 'hue' | 'shade';
-        index: number;
-        name: string;
-    };
+  type: ACTION_TYPES.RENAME;
+  data: {
+    layerType: 'hue' | 'shade';
+    index: number;
+    name: string;
+  };
 }
 export interface LoadPalleteActionRequest {
-    type: ACTION_TYPES.LOAD_PALLETE;
-    data: {
-        name: string;
-    };
+  type: ACTION_TYPES.LOAD_PALLETE;
+  data: {
+    name: string;
+  };
 }
 export interface SavePalleteActionRequest {
-    type: ACTION_TYPES.SAVE_PALLETE;
-    data: {
-        name: string;
-    };
+  type: ACTION_TYPES.SAVE_PALLETE;
+  data: {
+    name: string;
+  };
 }
 export interface ImportPalleteActionRequest {
-    type: ACTION_TYPES.IMPORT_PALLETE;
-    data: {
-        dataString: string;
-    };
+  type: ACTION_TYPES.IMPORT_PALLETE;
+  data: {
+    dataString: string;
+  };
 }
 export interface RebuildPalleteActionRequest {
-    type: ACTION_TYPES.REBUILD;
-    data: {};
+  type: ACTION_TYPES.REBUILD;
+  data: {};
 }
 export interface CalculateHueActionRequest {
-    type: ACTION_TYPES.CALCULATE_HUE;
-    data: {
-        hueIndex: number;
-    };
+  type: ACTION_TYPES.CALCULATE_HUE;
+  data: {
+    hueIndex: number;
+  };
 }
 export interface CalculateShadeActionRequest {
-    type: ACTION_TYPES.CALCULATE_SHADE;
-    data: {
-        shadeIndex: number;
-    };
+  type: ACTION_TYPES.CALCULATE_SHADE;
+  data: {
+    shadeIndex: number;
+  };
 }
 export type ActionOptions =
-    | SetColorActionRequest
-    | SetValActionRequest
-    | RenameActionRequest
-    | AddLayerActionRequest
-    | RemoveLayerActionRequest
-    | LoadPalleteActionRequest
-    | SavePalleteActionRequest
-    | ImportPalleteActionRequest
-    | RebuildPalleteActionRequest
-    | CalculateHueActionRequest
-    | CalculateShadeActionRequest;
+  | SetColorActionRequest
+  | SetValActionRequest
+  | RenameActionRequest
+  | AddLayerActionRequest
+  | RemoveLayerActionRequest
+  | LoadPalleteActionRequest
+  | SavePalleteActionRequest
+  | ImportPalleteActionRequest
+  | RebuildPalleteActionRequest
+  | CalculateHueActionRequest
+  | CalculateShadeActionRequest;
 export interface ActionRequest<A extends ActionOptions> {
-    type: A['type'];
-    data: A['data'];
+  type: A['type'];
+  data: A['data'];
 }
 export type reducerFunction<A extends ActionOptions> = (state: State, action: A) => State;
 interface SelectAction {
-    action: 'select';
-    tile: [number, number];
+  action: 'select';
+  tile: [number, number];
 }
 interface DeselectAction {
-    action: 'deselect';
-    tile?: [number, number];
+  action: 'deselect';
+  tile?: [number, number];
 }
 export type SelectionOptions = SelectAction | DeselectAction;
