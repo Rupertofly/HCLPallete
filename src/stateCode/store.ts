137b4c43cbf9fe2192a77d02f4a5a849aee3240c
Ruby Quail <ruby@rubyquail.design>
2020-06-04 16:19:23 +1000

    coupld more stateTransformers
    Signed-off-by: Ruby Quail <ruby@rubyquail.design>

--- src/stateCode/store.ts
+++ src/stateCode/store.ts
@@ -32,20 +32,6 @@ function nDimensionalReplaceAt<T, U extends nDimensionalArray<T>>(
 
   return recurse(source, 0) as U;
 }
-console.log(
-  nDimensionalReplaceAt(
-    [
-      [1, 2],
-      [3, 4],
-      [
-        [5, 6],
-        [7, 8],
-      ],
-    ],
-    [2, 0, 1],
-    (a: number) => 99
-  )
-);
 
 function circularMean(angles: number[]) {
   const mSin = d3.mean(angles.map((a) => Math.sin(fromDeg(a))));
@@ -83,6 +69,9 @@ function calculateColour(a: string | number, b?: number | string, cc?: number, i
     },
   };
 }
+
+// Colour Handlers
+
 export function handleSetVal(oldState: State, { options }: Actions.ActionSetValue['action']): State {
   const newState: State = {
     ...oldState,
@@ -98,9 +87,132 @@ export function handleSetVal(oldState: State, { options }: Actions.ActionSetValu
 
   return newState;
 }
-console.log(
-  handleSetVal(defaultState, {
-    type: Actions.COLOUR_ACTION_TYPES.SET_VAL,
-    options: { hue: 1, shade: 2, property: 'h', value: 69.69 },
-  }).colours[1]
-);
+export function handleSetColour(oldState: State, { options }: Actions.ActionSetColour['action']): State {
+  const newState: State = {
+    ...oldState,
+    colours: nDimensionalReplaceAt(oldState.colours, [options.hue, options.shade], (v: Colour) => {
+      if (typeof options.color === 'string') {
+        return calculateColour(options.color, v.id);
+      } else {
+        const { h, c, l } = options.color;
+
+        return calculateColour(h, c, l, v.id);
+      }
+    }),
+  };
+
+  return newState;
+}
+
+// Pallete Handlers
+
+function degDist(a: number, b: number) {
+  return Math.abs(b - a) < 180 ? Math.abs(b - a) : 360 - Math.abs(b - a);
+}
+function handleAddHueLayer(oldState: State, name: string): State {
+  const newHue = oldState.hues
+    .map((hu) => hu.avgHue)
+    .sort()
+    .reduce(
+      ({ min, ang }, v, i, arr) => {
+        let mVal = min;
+        let aVal = ang;
+        const l = arr.length;
+        const fwd = arr[(l + i + 1) % l];
+        const bk = arr[(l + i - 1) % l];
+        const fwdDistance = degDist(v, fwd);
+        const bkwdDistance = degDist(v, bk);
+
+        if (fwdDistance / 2 > mVal) {
+          mVal = fwdDistance / 2;
+          aVal = l + Math.sign(l - fwd) * mVal;
+        }
+        if (bkwdDistance / 2 > mVal) {
+          mVal = bkwdDistance / 2;
+          aVal = l + Math.sign(l - bk) * mVal;
+        }
+
+        return { min: mVal, ang: aVal };
+      },
+      { min: 0, ang: 0 }
+    ).ang;
+  const { colours } = oldState;
+
+  return {
+    ...oldState,
+    hues: [...oldState.hues, { name, avgHue: newHue, id: uniqueId('hue-') }],
+    colours: [
+      ...colours,
+      d3
+        .range(oldState.shades.length)
+        .map((si) =>
+          calculateColour(
+            newHue,
+            d3.mean(colours.map((hue) => hue[si].c)),
+            oldState.shades[si].avgValue,
+            uniqueId('col-')
+          )
+        ),
+    ],
+  };
+}
+function handleAddShadeLayer(oldState: State, name: string): State {
+  return {
+    ...oldState,
+    shades: [...oldState.shades, { name, avgValue: 45, id: uniqueId('shade-') }],
+    colours: oldState.colours.map((hue, hi) => [...hue, calculateColour(oldState.hues[hi].avgHue, 45, 45)]),
+  };
+}
+export function handleAddLayer(oldState: State, { options }: Actions.ActionAddLayer['action']): State {
+  switch (options.type) {
+    case 'hue':
+      return handleAddHueLayer(oldState, uniqueId('hueName'));
+    case 'shade':
+      return handleAddShadeLayer(oldState, uniqueId('shadeName'));
+  }
+}
+export function handleRemoveLayer(oldState: State, { options }: Actions.ActionRemoveLayer['action']): State {
+  switch (options.type) {
+    case 'hue':
+      return {
+        ...oldState,
+        hues: oldState.hues.filter((v, i) => i !== options.index),
+        colours: oldState.colours.filter((v, i) => i !== options.index),
+      };
+    case 'shade':
+      return {
+        ...oldState,
+        shades: oldState.shades.filter((v, i) => i !== options.index),
+        colours: oldState.colours.map((v) => v.filter((b, i) => i !== options.index)),
+      };
+    default:
+      return oldState;
+  }
+}
+function swapIndex<T>(source: T[], from: number, to: number): T[] {
+  const temp = source[to];
+  const newArr = source.slice(0);
+
+  newArr[to] = source[from];
+  newArr[from] = temp;
+
+  return newArr;
+}
+export function handleRearrangeLayer(oldState: State, { options }: Actions.ActionRearrangeLayer['action']): State {
+  switch (options.type) {
+    case 'hue':
+      return {
+        ...oldState,
+        hues: swapIndex(oldState.hues, options.from, options.to),
+        colours: swapIndex(oldState.colours, options.from, options.to),
+      };
+    case 'shade':
+      return {
+        ...oldState,
+        shades: swapIndex(oldState.shades, options.from, options.to),
+        colours: oldState.colours.map((h) => swapIndex(h, options.from, options.to)),
+      };
+    default:
+      return oldState;
+  }
+}
