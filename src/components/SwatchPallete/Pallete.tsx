217060289ad85ddef4c0d1573990549f15192a7a
Ruby Quail <ruby@rubyquail.design>
2020-06-07 20:52:01 +1000

    ugh
    Signed-off-by: Ruby Quail <ruby@rubyquail.design>

--- src/components/SwatchPallete/Pallete.tsx
+++ src/components/SwatchPallete/Pallete.tsx
@@ -3,6 +3,7 @@ import * as T from '../../types';
 import * as S from '../../stateCode';
 import * as ED from 'riek';
 import Swatch from './Swatch';
+import PH from './PalleteHeader';
 interface Props {
   st: S.State;
   selected: [number, number];
@@ -13,61 +14,65 @@ interface Props {
 export function Pallete({ st, select, selected, dispatch }: Props) {
   return (
     <table style={{ borderSpacing: 0, fontFamily: 'fira code' }}>
-      <tr>
-        <th />
-        {st.shades.map((sh, i) => {
-          return (
-            <th
-              style={{
-                backgroundColor: i === selected[1] ? '#e0e0e0ff' : '#fefefe00',
+      <tbody>
+        <tr>
+          <th />
+          {st.shades.map((sh, i) => {
+            return (
+              <th
+                style={{
+                  backgroundColor: i === selected[1] ? '#e0e0e0ff' : '#fefefe00',
 
-                transition: 'background-color 233ms',
-              }}
-              key={i}>
-              {' '}
-              <input
-                type='textarea'
-                name='shades'
-                value={sh.name}
-                size={6}
-                style={{ border: 0, fontFamily: 'fira code', background: '0' }}
-              />
-            </th>
+                  transition: 'background-color 233ms',
+                }}
+                key={i}>
+                {' '}
+                <input
+                  type='textarea'
+                  name='shades'
+                  value={sh.name}
+                  size={6}
+                  style={{ border: 0, fontFamily: 'fira code', background: '0' }}
+                />
+              </th>
+            );
+          })}
+        </tr>
+        {st.colours.map((hue, hueIndex) => {
+          return (
+            <tr key={hueIndex}>
+              <PH
+                info={st.hues[hueIndex]}
+                index={hueIndex}
+                dispatch={dispatch}
+                style={{
+                  backgroundColor: hueIndex === selected[0] ? '#e0e0e0ff' : '#fefefe00',
+
+                  transition: 'background-color 233ms',
+                }}></PH>
+              {hue.map((col, shadeIndex) => {
+                return (
+                  <td
+                    style={{
+                      backgroundColor:
+                        hueIndex === selected[0] || shadeIndex === selected[1] ? '#e0e0e0ff' : '#fefefe00',
+
+                      transition: 'background-color 233ms',
+                    }}>
+                    <Swatch
+                      key={shadeIndex}
+                      color={col}
+                      location={[hueIndex, shadeIndex]}
+                      selected={hueIndex === selected[0] && shadeIndex === selected[1]}
+                      selector={select}
+                    />
+                  </td>
+                );
+              })}
+            </tr>
           );
         })}
-      </tr>
-      {st.colours.map((hue, hueIndex) => {
-        return (
-          <tr key={hueIndex}>
-            <th
-              style={{
-                backgroundColor: hueIndex === selected[0] ? '#e0e0e0ff' : '#fefefe00',
-
-                transition: 'background-color 233ms',
-              }}>
-              {st.hues[hueIndex].name}
-            </th>
-            {hue.map((col, shadeIndex) => {
-              return (
-                <td
-                  style={{
-                    backgroundColor: hueIndex === selected[0] || shadeIndex === selected[1] ? '#e0e0e0ff' : '#fefefe00',
-
-                    transition: 'background-color 233ms',
-                  }}>
-                  <Swatch
-                    key={shadeIndex}
-                    color={col}
-                    location={[hueIndex, shadeIndex]}
-                    selected={hueIndex === selected[0] && shadeIndex === selected[1]}
-                    selector={select}
-                  />
-                </td>
-              );
-            })}
-          </tr>
-        );
-      })}
+      </tbody>
     </table>
   );
 }
