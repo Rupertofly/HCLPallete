14007c00dd75e60aa3376aa2a3dc1086f78daf07
Ruby Quail <ruby@rubyquail.design>
2020-11-19 21:55:39 +1100

    bit of cleanup
    Signed-off-by: Ruby Quail <ruby@rubyquail.design>

---
+++ src/app.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { template } from 'lodash';
+
+  let e: string = '0';
+</script>
+
+<style lang="scss">
+  * {
+    background-color: red;
+  }
+</style>
+
+<!-- markup (zero or more items) goes here -->
+
+<template>
+  <section />
+</template>
